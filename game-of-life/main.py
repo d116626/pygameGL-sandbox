5fc5466a64541364089bb4c7dbc86e1bed5cfd20
d116626 <d116626@gmail.com>
2023-09-13 23:22:50 -0300

    chore: refactor to use classes

--- game-of-life/main.py
+++ game-of-life/main.py
@@ -1,83 +1,130 @@
-import pygame
-from random import randint
-from copy import deepcopy
+import pygame as pg
 import numpy as np
 from numba import njit
 
 RES = WIDTH, HEIGHT = 1500, 900
-TILE = 4
+TILE = 2
 W, H = WIDTH // TILE, HEIGHT // TILE
-FPS = 20
-
-pygame.init()
-surface = pygame.display.set_mode(RES)
-clock = pygame.time.Clock()
+FPS = 30
 
 
-next_field = np.zeros((H, W), dtype=int)
-current_field = deepcopy(next_field)
-# Calculate the dimensions of the X pattern
-x_length = min(W, H) // 2  # Half the size of the X pattern
+class GameOfLife:
+    def __init__(self):
+        self.current_field = np.zeros((H, W), dtype=int)
+        self.next_field = np.zeros((H, W), dtype=int)
+        self.is_game_running = False
 
-# Calculate the center coordinates
-center_x = W // 2
-center_y = H // 2
+        x_length = min(W, H) // 2
+        center_x = W // 2
+        center_y = H // 2
 
-# Create the X pattern
-for i in range(-x_length, x_length + 1):
-    x1 = center_x + i
-    y1 = center_y + i
-    x2 = center_x - i
-    y2 = center_y + i
-    
-    # Check bounds to avoid going outside the grid
-    if 0 <= x1 < W and 0 <= y1 < H:
-        current_field[y1, x1] = 1
-    if 0 <= x2 < W and 0 <= y2 < H:
-        current_field[y2, x2] = 1
+        for i in range(-x_length, x_length + 1):
+            x1 = center_x + i
+            y1 = center_y + i
+            x2 = center_x - i
+            y2 = center_y + i
 
+            if 0 <= x1 < W and 0 <= y1 < H:
+                self.current_field[y1, x1] = 1
+            if 0 <= x2 < W and 0 <= y2 < H:
+                self.current_field[y2, x2] = 1
 
+    def toggle_running(self):
+        self.is_game_running = not self.is_game_running
 
-@njit(fastmath=True)
-def check_cell(current_field, next_field):
-    res = []
-    for x in range(1,W-1):
-        for y in range(1,H-1):
-            count=0
-            for j in range(y-1,y+2):
-                for i in range(x-1,x+2):
-                    if current_field[j][i]==1:
-                        count+=1
-            if current_field[y][x]==1:
-                count-=1
-                if count==2 or count==3:
-                    next_field[y][x]=1
-                    res.append((x,y))
+    @staticmethod
+    @njit(fastmath=True)
+    def check_cell(current_field, next_field):
+        res = []
+        for x in range(1, W - 1):
+            for y in range(1, H - 1):
+                count = 0
+                for j in range(y - 1, y + 2):
+                    for i in range(x - 1, x + 2):
+                        if current_field[j][i] == 1:
+                            count += 1
+                if current_field[y][x] == 1:
+                    count -= 1
+                    if count == 2 or count == 3:
+                        next_field[y][x] = 1
+                        res.append((x, y))
+                    else:
+                        next_field[y][x] = 0
                 else:
-                    next_field[y][x]=0
-            else:
-                if count==3:
-                    next_field[y][x]=1
-                    res.append((x,y))
-                else:
-                    next_field[y][x]=0
-    return next_field, res
+                    if count == 3:
+                        next_field[y][x] = 1
+                        res.append((x, y))
+                    else:
+                        next_field[y][x] = 0
+        return next_field, res
+
+    def update(self):
+        if self.is_game_running:
+            self.next_field, res = self.check_cell(self.current_field, self.next_field)
+            self.current_field = np.copy(self.next_field)
+            self.res = res
+
+    def draw(self, surface):
+        surface.fill(pg.Color("black"))
+
+        for y in range(H):
+            for x in range(W):
+                if self.current_field[y][x] == 1:
+                    pg.draw.rect(
+                        surface,
+                        pg.Color("darkorange"),
+                        (x * TILE + 1, y * TILE + 1, TILE - 1, TILE - 1),
+                    )
+
+        [
+            pg.draw.line(surface, pg.Color("black"), (x, 0), (x, HEIGHT))
+            for x in range(0, WIDTH, TILE)
+        ]
+        [
+            pg.draw.line(surface, pg.Color("black"), (0, y), (WIDTH, y))
+            for y in range(0, HEIGHT, TILE)
+        ]
+
+        if hasattr(self, "res"):
+            [
+                pg.draw.rect(
+                    surface,
+                    pg.Color("darkorange"),
+                    (x * TILE + 1, y * TILE + 1, TILE - 1, TILE - 1),
+                )
+                for x, y in self.res
+            ]
 
 
+class App:
+    def __init__(self):
+        pg.init()
+        self.surface = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.game_of_life = GameOfLife()
 
-while True:
-    surface.fill(pygame.Color('black'))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+    def run(self):
+        while True:
+            self.handle_events()
+            self.game_of_life.update()
+            self.draw()
+            self.clock.tick(FPS)
 
-    [pygame.draw.line(surface, pygame.Color('darkslategray'), (x, 0), (x, HEIGHT)) for x in range(0, WIDTH, TILE)]
-    [pygame.draw.line(surface, pygame.Color('darkslategray'), (0, y), (WIDTH, y)) for y in range(0, HEIGHT, TILE)]
-    # draw life
-    next_field, res = check_cell(current_field, next_field)
-    [pygame.draw.rect(surface, pygame.Color('darkorange'), (x * TILE + 1, y * TILE + 1, TILE - 1, TILE - 1)) for x, y in res]
-    current_field = deepcopy(next_field)
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.game_of_life.toggle_running()
 
-    # print(clock.get_fps())
-    pygame.display.flip()
-    clock.tick(FPS)
+    def draw(self):
+        self.surface.fill(pg.Color("black"))
+        self.game_of_life.draw(self.surface)
+        pg.display.flip()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
